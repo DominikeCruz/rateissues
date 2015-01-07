7af8bb9f9e8cdbfa80a450fbed370117a07eeb99
Cassio Cabral <cassioscabral@gmail.com>
2015-01-07 14:22:45 -0300

    add search page and home page, updated routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def search
+    # fazer uma busca baseada no params[:language]
+    @language = params[:language]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/index'
-
+  get 'search/:language', to: 'pages#search', as: "search"
   root to: 'pages#index'
 end
