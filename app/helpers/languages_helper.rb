28b2a9e760d91461fbd38863d3355f8618afde5d
Cassio Cabral <cassioscabral@gmail.com>
2015-01-09 18:25:05 -0300

    add languages.yml with all languages found in github linguist; Listing top languages in index.

---
+++ app/helpers/languages_helper.rb
@@ -0,0 +1,7 @@
+module LanguagesHelper
+  def top_languages
+    ["Ruby", "C", "Python", "PHP", "Julia", "Rust", "Java", "Scala", "Elixir", "Objective-C",
+     "LiveScript", "Haskell", "Scheme", "CoffeeScript", "Nimrod", "F#",
+     "JavaScript", "Racket", "Erlang", "Go"]
+  end
+end
